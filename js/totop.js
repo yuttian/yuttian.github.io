a733d871e8b7b7d008c7eae71b9f00a7abc99153
yuttian <532388099@qq.com>
2017-08-15 22:12:22 +0800

    Site updated: 2017-08-15 22:12:22

---
+++ js/totop.js
@@ -0,0 +1,24 @@
+$(document).ready((function(_this) {
+    return function() {
+        var bt;
+        bt = $('#back_to_top');
+        if ($(document).width() > 480) {
+        $(window).scroll(function() {
+            var st;
+            st = $(window).scrollTop();
+            console.log("st",st);
+            if (st > 30) {
+              return bt.css('display', 'block');
+            } else {
+              return bt.css('display', 'none');
+            }
+        });
+        return bt.click(function() {
+            $('body,html').animate({
+            scrollTop: 0
+            }, 800);
+            return false;
+        });
+        }
+    };
+})(this));
